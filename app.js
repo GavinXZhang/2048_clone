48ddf229b3a42c5573d4950eb7af398dbce0df68
Gavin Zhanggit config --global user.email gwxzhang@bu.edu <gwxzhang@bu.edu>
2025-07-10 02:01:11 -0400

    intial 2048 look

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDispaly = document.querySelector('score')
+    const resultDisplay = document.getElementById('result')
+
+    console.log(gridDisplay)
+
+})
