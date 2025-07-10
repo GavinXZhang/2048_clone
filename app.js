3967f8ff0d36f72817ffe667dcf9557fe693a87e
Gavin Zhanggit config --global user.email gwxzhang@bu.edu <gwxzhang@bu.edu>
2025-07-10 19:08:44 -0400

    added keys left and keys right button, combined if the two values are the same are slides it to the end, update with score

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 4
     const resultDisplay = document.getElementById('#result')
     let squares = []
+    let score = 0;
 
     function createBoard() {
         for (let i = 0; i < width * width; i++){
@@ -42,8 +43,43 @@ document.addEventListener('DOMContentLoaded', () => {
                     squares[i + 1].innerHTML = newRow[1]
                     squares[i + 2].innerHTML = newRow[2]
                     squares[i + 3].innerHTML =  newRow[3]
+                    
                 }
             }
+        }
+        function moveLeft(){
+            for (let i = 0; i < 16; i ++){
+                if (i % 4 ===0 ){
+                    let totalone = squares[i].innerHTML
+                    let totalTwo = squares[i + 1].innerHTML
+                    let totalThree = squares[i + 2].innerHTML
+                    let totalFour = squares[i + 3].innerHTML
+
+                    let row = [parseInt(totalone), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                    let filteredRow = row.filter(num => num)
+                    let missing = 4 - filteredRow.length
+                    let zeros = Array(missing).fill(0)
+                    let newRow = filteredRow.concat(zeros)
+                    squares[i].innerHTML = newRow[0]
+                    squares[i + 1].innerHTML = newRow[1]
+                    squares[i + 2].innerHTML = newRow[2]
+                    squares[i + 3].innerHTML =  newRow[3]
+                }
+            }
+        }
+
+        function combineRow() {
+            for (let i = 0; i < 15; i++){
+                if(squares[i].innerHTML === squares[i+1].innerHTML){
+                    let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i+1].innerHTML)
+                    squares[i].innerHTML = combinedTotal
+                    squares[i+1].innerHTML = 0
+                    score += combinedTotal
+                    scoreDispaly.innerHTML = score
+                }
+            }
+            // checkForWin()
         }
         function control (e) {
             if (e.key === 'ArrowLeft') {
@@ -55,15 +91,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         document.addEventListener('keydown', control)
 
-        // function keyLeft(){
-        //     moveLeft()
-        //     combineRow()
-        //     moveLeft()
-        //     generate()
-        // }
+        function keyLeft(){
+            moveLeft()
+            combineRow()
+            moveLeft()
+            generate()
+        }
         function keyRight(){
             moveRight()
-            // combineRow()
+            combineRow()
             moveRight()
             generate()
         }
